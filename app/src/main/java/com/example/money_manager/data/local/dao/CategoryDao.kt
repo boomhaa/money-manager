cb7b7a417d248fa652168d1e6c7a9c7a741aa809
boomhaa~ <traker566@gmail.com>
2025-09-03 17:23:53 +0300

    #9-realize methods for category dao

--- app/src/main/java/com/example/money_manager/data/local/dao/CategoryDao.kt
+++ app/src/main/java/com/example/money_manager/data/local/dao/CategoryDao.kt
@@ -1,5 +1,23 @@
 package com.example.money_manager.data.local.dao
 
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.money_manager.data.local.entity.CategoryEntity
+import kotlinx.coroutines.flow.Flow
+
 interface CategoryDao {
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCategory(category: CategoryEntity)
+
+    @Delete
+    suspend fun deleteCategory(category: CategoryEntity)
+
+    @Query("SELECT * FROM categories ORDER BY name ASC")
+    suspend fun getAllCategories(): Flow<List<CategoryEntity>>
+
+    @Query("SELECT * FROM categories WHERE id = :id LIMIT 1")
+    suspend fun getCategoryById(id: Long): CategoryEntity
 }
