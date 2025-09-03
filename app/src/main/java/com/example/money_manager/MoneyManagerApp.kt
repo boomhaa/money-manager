dfb8eba406b6f13d576e2272c41a4d274452f726
boomhaa~ <traker566@gmail.com>
2025-09-03 14:54:15 +0300

    #2-hilt and manifest file setup

---
+++ app/src/main/java/com/example/money_manager/MoneyManagerApp.kt
@@ -0,0 +1,7 @@
+package com.example.money_manager
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MoneyManagerApp : Application()
---
+++ app/src/main/java/com/example/money_manager/di/AppModule.kt
@@ -0,0 +1,10 @@
+package com.example.money_manager.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+}
