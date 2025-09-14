e6848cc84be0a2e21fae6068085be6087be03166
boomhaa~ <90379111+boomhaa@users.noreply.github.com>
2025-09-15 00:05:57 +0300

    #37-update choosing category functional and make some decor

--- app/src/main/java/com/example/money_manager/domain/model/MenuItem.kt
+++ app/src/main/java/com/example/money_manager/domain/model/MenuItem.kt
@@ -1,6 +1,9 @@
 package com.example.money_manager.domain.model
 
+import androidx.compose.ui.graphics.vector.ImageVector
+
 data class MenuItem(
     val label: String,
-    val route: String
+    val route: String,
+    val icon: ImageVector? = null
 )
--- app/src/main/java/com/example/money_manager/utils/ScreenMenuList.kt
+++ app/src/main/java/com/example/money_manager/utils/ScreenMenuList.kt
@@ -1,12 +1,15 @@
 package com.example.money_manager.utils
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material.icons.filled.*
 import com.example.money_manager.domain.model.MenuItem
 import com.example.money_manager.presentation.navigation.Screens
 
 object ScreenMenuList {
     val screenMenuList: List<MenuItem> = listOf(
-        MenuItem("Главная", Screens.Home.route),
-        MenuItem("Статистика", Screens.Statistics.route),
-        MenuItem("Категории", Screens.Categories.route)
+        MenuItem("Главная", Screens.Home.route, Icons.Filled.Home),
+        MenuItem("Статистика", Screens.Statistics.route, Icons.Filled.BarChart),
+        MenuItem("Категории", Screens.Categories.route, Icons.AutoMirrored.Filled.List)
     )
 }
